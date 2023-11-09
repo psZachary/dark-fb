22b0825834eabef5e2b309ac368a774ebf5dc2e0
Zachary Lesler <zacklesler@outlook.com>
2023-11-08 20:50:28 -0500

    Clarification

--- offsets.h
+++ offsets.h
@@ -2,9 +2,12 @@
 
 #include <cinttypes>
 
-// https://www.youtube.com/watch?v=GWZQiddlnVM (Made by me)
+// https://www.youtube.com/watch?v=GWZQiddlnVM 
+// (Easiest one to find by far, YouTube video made by me explains the process very well)
 constexpr std::uint64_t GWORLD   = 0x7BE69D0;
-// MulticastDelegateProperty (search string in Cheat Engine)
+// "MulticastDelegateProperty"
+// (search string in Cheat Engine & find start of FNamePool & get static pointer then subtract 0x10)
 constexpr std::uint64_t GNAMES   = 0x79D8F00;
-// 48 8B 05 ? ? ? ? 48 8B 0C C8 48 8D 04 D1 EB
+// 48 8B 05 ? ? ? ? 48 8B 0C C8 48 8D 04 D1 EB 
+// (Find sig in IDA)
 constexpr std::uint64_t GOBJECTS = 0x7A78890;
