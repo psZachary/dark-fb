15ee848871bb0baacc8454b46dfeac5f7ffc3b78
bditt <ProjectElitist@gmail.com>
2023-12-19 19:13:09 -0500

    Cleaned up code.

--- main.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <iostream>
-#include <thread>
-#include <chrono>
-
-#define fail(message) std::cout << message << std::endl; std::this_thread::sleep_for(std::chrono::seconds(3)); exit(0);
